792559573895381ce9c5037073d5322ff4a6c40b
BayyaChinnu4202 <158019316+BayyaChinnu4202@users.noreply.github.com>
2026-02-20 21:07:01 +0530

    Add files via upload

--- Grocery App/index.js
+++ Grocery App/index.js
@@ -1,18 +1,84 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import './style.css'; // Import your custom CSS for styling
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
+const Dashboard = () => {
+  const [data, setData] = useState({
+    products: 0,
+    users: 0,
+    orders: 0,
+  });
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>
-);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [usersResponse, productsResponse, ordersResponse] = await Promise.all([
+          axios.get('http://localhost:5100/users'),
+          axios.get('http://localhost:5100/products'),
+          axios.get('http://localhost:5100/orders'),
+        ]);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        setData({
+          users: usersResponse.data.length,
+          products: productsResponse.data.length,
+          orders: ordersResponse.data.length,
+        });
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="dashboard container">
+      <h2>Dashboard</h2>
+
+      <div className="card-container">
+        <Card>
+          <Card.Body>
+            <Card.Title>Product Count</Card.Title>
+            <Card.Text>{data.products} Products</Card.Text>
+            <Link style={{textDecoration:'none',color:'white'}} to={'/admin/all-products'}><Button variant="primary">View Products </Button></Link>
+          </Card.Body>
+        </Card>
+
+        <Card>
+          <Card.Body>
+            <Card.Title>User Count</Card.Title>
+            <Card.Text>{data.users} Users</Card.Text>
+            <Link style={{textDecoration:'none',color:'white'}} to={'/admin/users'}><Button variant="primary">View Users </Button></Link>
+          </Card.Body>
+        </Card>
+
+        <Card>
+          <Card.Body>
+            <Card.Title>Order Count</Card.Title>
+            <Card.Text>{data.orders} Orders</Card.Text>
+            <Link style={{textDecoration:'none',color:'white'}} to="/admin/orders"><Button variant="primary">View Orders</Button></Link>
+          </Card.Body>
+        </Card>
+
+        <Card>
+          <Card.Body>
+            <Card.Title>Add Product</Card.Title>
+            <Link style={{textDecoration:'none',color:'white'}} to='/admin/add-product'><Button style={{width:'100px'}} variant="success">Add </Button></Link>
+          </Card.Body>
+        </Card>
+
+        <Card>
+          <Card.Body>
+            <Card.Title>Add Category</Card.Title>
+            <Link style={{textDecoration:'none',color:'white',}} to='/admin/add-category'><Button style={{width:'100px'}} variant="success">Add </Button></Link>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
